4432f72a073575fcc3194c885498d8605f6ced8a
Seth T <sethten@users.noreply.github.com>
2017-02-06 15:29:28 -0500

    Add waitForPress helper method

--- EVShield_examples/reaction_time/reaction_time.ino
+++ EVShield_examples/reaction_time/reaction_time.ino
@@ -7,6 +7,11 @@ const long maxTime = 5000;
 EVShield ev;
 EVs_NXTTouch touch;
 
+void waitForPress() {
+    while (!touch.isPressed()) delay(10); // wait until the touch sensor is pressed
+    while (touch.isPressed()) delay(10); // wait until it is released
+}
+
 void setup() {
     ev.init();
     touch.init(&ev, SH_BAS1);
@@ -14,16 +19,15 @@ void setup() {
     
     ev.screen.setTextSize(4); // large for title
     ev.screen.println("Ready?");
+    
     ev.screen.setTextSize(2); // default (1 is very small)
     // prepend a newline to put some space between the title and this text
     // this text will automatically be wrapped to the next line on screen
     ev.screen.println("\nAttach a touch sensor to  BAS1 and press to begin.");
-    
-    while (!touch.isPressed()) delay(10); // wait until the touch sensor is pressed
-    while (touch.isPressed()) delay(10); // wait until it is released
+    waitForPress();
     
     ev.screen.println("\nWait for the screen to turn red, then press!");
-    delay(4000);
+    waitForPress();
     
     ev.screen.clearScreen();
 }
@@ -40,19 +44,11 @@ void loop() {
     while (!touch.isPressed()) delay(10);
     long end = millis();
     
+    ev.screen.setCursor(0,0);
     ev.screen.print("You reacted in ");
     ev.screen.print(end - start);
-    ev.screen.println(" milliseconds!");
+    ev.screen.println(" ms!");
     
-    delay(5000);
+    waitForPress();
     ev.screen.clearScreen();
-    
-    // ev.screen.setCursor(0,0);
-    // ev.screen.print(touch.readRaw());
-    // ev.screen.print("    ");
-    
-    // if (touch.isPressed())
-    //     ev.screen.fillRect(10, 10, 20, 10, ILI9341_RED);
-    // else
-    //     ev.screen.fillRect(10, 10, 20, 10, ILI9341_WHITE);
 }
